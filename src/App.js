3cd397562640ca6e99a420d826795f43285a57d0
punam102 <jagtappunam129@gmail.com>
2023-01-23 19:30:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-// import Home from './Pages/Home';
-import Allroutes from './Pages/Allroutes';
-import Navbar from './Pages/Navbar';
+import "./App.css";
+import Allroutes from "./Pages/Allroutes";
+import Navbar from "./Pages/Navbar";
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>🐶Puppy love</h1> */}
-      <Navbar/>
-      {/* <Home/> */}
-      <Allroutes/>
+      <Navbar />
+
+      <Allroutes />
     </div>
   );
 }
